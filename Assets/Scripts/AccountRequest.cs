360c2e48f273cfe0869804e1775584bde877448c
Kuni <binlen11112@gmail.com>
2026-03-01 23:19:59 +0700

    done api login and ui, done register api not ui

---
+++ Assets/Scripts/AccountRequest.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AccountRequest
+{
+    public string Username;
+    public string Password;
+}
---
+++ Assets/Scripts/AccountResponse.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AccountResponse
+{
+    public string accountId;
+    public string accountName;
+    public int score;
+}
